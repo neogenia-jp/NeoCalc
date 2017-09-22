6dea3a0612e1e8f0b033dde8e5baa59f83a4d331
h.moriguchi <h.moriguchi@neogenia.co.jp>
2017-09-22 14:28:56 +0900

    ボタンをサービスに追記

--- CalcLib/CalcSvc.cs
+++ CalcLib/CalcSvc.cs
@@ -20,16 +20,38 @@ namespace CalcLib
 
             switch (btn)
             {
+                //演算子
                 case CalcButton.BtnPlus:
                     ctx.Ope = btn;
                     break;
-                case CalcButton.BtnMinux:
+                case CalcButton.BtnMinus:
                     ctx.Ope = btn;
                     break;
+                case CalcButton.BtnDivide:
+                    ctx.Ope = btn;
+                    break;
+                case CalcButton.BtnMultiple:
+                    ctx.Ope = btn;
+                    break;
+
+                //計算
                 case CalcButton.BtnEqual:
                     answer = Calc(ctx.Value1, ctx.Value2, ctx.Ope.Value);
                     ctx.DisplayText = answer.ToString();
                     break;
+
+                //クリア
+                case CalcButton.BtnClear:
+
+                    break;
+                case CalcButton.BtnClearEnd:
+
+                    break;
+                case CalcButton.BtnBS:
+
+                    break;
+
+                //値入力
                 default:
                     if (ctx.Ope == null)
                     {
@@ -44,6 +66,9 @@ namespace CalcLib
                     break;
             }
 
+
+            //出力
+
             //Displayには一回一回結果が表示されている
             //ctx.DisplayText 
 
@@ -51,6 +76,13 @@ namespace CalcLib
             //ctx.SubDisplayText 
         }
 
+        /// <summary>
+        /// 計算処理
+        /// </summary>
+        /// <param name="Value1"></param>
+        /// <param name="Value2"></param>
+        /// <param name="Ope"></param>
+        /// <returns></returns>
         public int Calc(string Value1, string Value2, CalcButton Ope)
         {
             int answer = 0;
@@ -61,11 +93,18 @@ namespace CalcLib
             if (Ope == CalcButton.BtnPlus)
             {
                 answer = val1 + val2;
-
             }
-            else if (Ope == CalcButton.BtnMinux)
+            else if (Ope == CalcButton.BtnMinus)
             {
                 answer = val1 - val2;
+            }
+            else if (Ope == CalcButton.BtnDivide)
+            {
+                answer = val1 * val2;
+            }
+            else if (Ope == CalcButton.BtnMultiple)
+            {
+                answer = val1 / val2;
             }
             return answer;
         }
