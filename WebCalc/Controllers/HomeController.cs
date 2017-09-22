68586676c620ea1aac1fcc1c05218ac3a542525e
Wataru Maeda <w.maeda@neogenia.co.jp>
2017-09-22 12:49:33 +0900

    Web版のUIを追加

---
+++ WebCalc/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using CalcLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index() => View();
+
+        [HttpPost]
+        public ActionResult Index(string btn)
+        {
+            var ctx = Session["Context"] as ICalcContext;
+            if (ctx == null)
+            {
+                ctx = Factory.CreateContext();
+                Session["Context"] = ctx;
+            }
+
+            var btnName = $"Btn{btn}";
+            CalcButton cb;
+            if (!Enum.TryParse(btnName, out cb))
+            {
+                throw new ApplicationException($"ボタン'{btnName}'が解釈できません。");
+            }
+
+            var svc = Factory.CreateService();
+            svc.OnButtonClick(ctx, cb);
+
+            return new JsonResult() { Data = ctx };
+        }
+    }
+}
