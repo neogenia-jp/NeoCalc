cbe5a9b9e3eb1f4d7c4c7ae69cfbf439daab5507
Wataru Maeda <w.maeda@neogenia.co.jp>
2017-09-22 13:26:46 +0900

    小数演算誤差のテストを追加

---
+++ CalcLibTest/CalcSvcTest2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcLibTest
+{
+    [TestClass]
+    public class CalcSvcTest2
+    {
+        [TestMethod]
+        public void 小数の2進数演算誤差のテスト()
+        {
+            var ctx = CalcLib.Factory.CreateContext();
+            var svc = CalcLib.Factory.CreateService();
+
+            // 30000
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn3);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn0);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn0);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn0);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn0);
+
+            // ×
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.BtnMultiple);
+
+            // 0.0021
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn0);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.BtnDot);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn0);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn0);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn2);
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn1);
+
+            // =
+            svc.OnButtonClick(ctx, CalcLib.CalcButton.BtnEqual);
+
+
+            Assert.AreEqual("63", ctx.DisplayText);
+        }
+    }
+}
