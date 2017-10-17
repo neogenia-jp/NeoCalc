df494a53415c81caab7055e713566951d7752aea
Wataru Maeda <w.maeda@neogenia.co.jp>
2017-10-18 02:16:31 +0900

    小数の有効桁数のテストを追加、修正

--- CalcLibTest/CalcSvcTest.cs
+++ CalcLibTest/CalcSvcTest.cs
@@ -163,8 +163,8 @@ namespace CalcLibTest
             svc.OnButtonClick(ctx, CalcLib.CalcButton.Btn3);
             svc.OnButtonClick(ctx, CalcLib.CalcButton.BtnEqual);
 
-            // double型の有効桁数が15～16桁のため16桁としておく
-            Assert.AreEqual("0.333333333333333", ctx.DisplayText);
+            // 小数点以下は13桁までとする
+            Assert.AreEqual("0.3333333333333", ctx.DisplayText);
         }
     }
 }
