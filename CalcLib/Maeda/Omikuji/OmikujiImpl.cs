96c2aa7f76c966ba04038510beafaa0976353cc6
Wataru Maeda <w.maeda@neogenia.co.jp>
2017-10-23 13:12:50 +0900

    おみくじメインロジックの実装

---
+++ CalcLib/Maeda/Omikuji/OmikujiImpl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalcLib.Maeda.Util;
+
+namespace CalcLib.Maeda.Omikuji
+{
+    public struct Kuji
+    {
+        public string Name { get; }
+        public int Worth { get; }
+        public Kuji(int worth, string name) { Name = name; Worth = worth; }
+        public override string ToString() => (Name+"　").Substring(0, 2);
+    }
+
+    [Serializable]
+    public class OmikujiImpl
+    {
+        public IList<Kuji> Items { get; private set; }
+
+        public int SelctedIdx { get; private set; } = -1;
+
+        public void Init()
+        {
+            Items = new List<Kuji>
+            {
+                new Kuji(-10, "凶"),
+                new Kuji(  3, "小吉"),
+                new Kuji(  6, "中吉"),
+                new Kuji( 10, "大吉"),
+            }.Shuffle();
+            SelctedIdx = -1;
+        }
+
+        public bool TryChoise(int num)
+        {
+            if (!IsFinished && 0 <= num && num < Items.Count)
+            {
+                SelctedIdx = num;
+                return true;
+            }
+            return false;
+        }
+
+        public string ResultText => $"あなたの運勢は「{Items[SelctedIdx]}」です";
+
+        public bool IsFinished => SelctedIdx >= 0;
+    }
+}
---
+++ CalcLib/Maeda/Omikuji/OmikujiSvc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CalcLib.Maeda.Basis;
+
+namespace CalcLib.Maeda
+{
+    /// <summary>
+    /// おみくじサービスのためのコンテキスト
+    /// </summary>
+    internal class OmikujiContext : ICalcContext
+    {
+        public string DisplayText { get; set; }
+
+        public string SubDisplayText { get; set; }
+
+        public Omikuji.OmikujiImpl omikuji { get; set; } = new Omikuji.OmikujiImpl();
+
+        public bool finished;
+
+        public void Init()
+        {
+            DisplayText = "[1 ] [2 ] [3 ] [4 ]";
+            SubDisplayText = "";
+            omikuji.Init();
+        }
+
+        public bool Choise(int n)
+        {
+            if (!omikuji.TryChoise(n)) return false;  // 選択不可なら何もせずfalseを返す
+
+            DisplayText = string.Join(" ", omikuji.Items);
+            SubDisplayText = omikuji.ResultText;
+            return true;
+        }
+
+        public bool Finished => omikuji.IsFinished;
+    }
+
+    /// <summary>
+    /// おみくじサービス
+    /// </summary>
+    internal class OmikujiSvc : SvcBase<OmikujiContext>
+    {
+        public override string GetExtButtonText(int num) => num == 1 ? "*" : null;
+
+        protected override OmikujiContext _CreateContext() => new OmikujiContext();
+
+
+        public override bool TryButtonClick(OmikujiContext ctx, CalcButton btn)
+        {
+            switch (btn)
+            {
+                case CalcButton.BtnExt1:
+                    ctx.Init();
+                    break;
+                default:
+                    if (ctx.Finished) return false;
+                    ctx.Choise(btn - CalcButton.Btn1);
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
