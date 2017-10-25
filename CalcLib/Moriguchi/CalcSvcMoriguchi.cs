abff25e5e42c45395a01d9f62558cc127329decc
h.moriguchi <h.moriguchi@neogenia.co.jp>
2017-10-25 19:49:23 +0900

    小数点削除時のバグ対応

--- CalcLib/Moriguchi/CalcSvcMoriguchi.cs
+++ CalcLib/Moriguchi/CalcSvcMoriguchi.cs
@@ -164,7 +164,7 @@ namespace CalcLib.Moriguchi
                 case CalcButton.BtnDivide:
                 case CalcButton.BtnMultiple:
                     //小数点押下直後に演算子を押下すると小数点を削除する
-                    if (ctx.Buffer.EndsWith("."))
+                    if (ctx.Buffer?.EndsWith(".") == true)
                     {
                         char[] dot = { '.' };
                         ctx.Buffer = ctx.Buffer.TrimEnd(dot);
@@ -212,7 +212,7 @@ namespace CalcLib.Moriguchi
 
                 //小数点押下時
                 case CalcButton.BtnDot:
-                    if (!ctx.Buffer.EndsWith(".")) ctx.Buffer += ".";
+                    if (!ctx.Buffer?.EndsWith(".") == true) ctx.Buffer += ".";
                     break;
 
                 //計算
