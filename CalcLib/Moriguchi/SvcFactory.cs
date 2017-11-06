c6f19a624745695ad3a0c7163b5ced4f658a54e5
h.moriguchi <h.moriguchi@neogenia.co.jp>
2017-11-06 22:12:44 +0900

    サービスを作る為のインターフェースとFactoryを追加。

---
+++ CalcLib/Moriguchi/SvcFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLib.Moriguchi
+{
+    public static class SvcFactory
+    {
+        public static ISubContext CreateContext() => CreateService().CreateContext();
+
+        public static ISubSvc CreateService()
+        {
+            ISubSvc o = null;
+            return o;
+        }
+    }
+}
