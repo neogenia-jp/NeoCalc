567de86950f6aaaf4a7f2f3c601af922062cfbb3
Wataru Maeda <w.maeda@neogenia.co.jp>
2017-11-08 21:15:45 +0900

    株価取得サービスの実装

---
+++ CalcLib/Maeda/Finance/FinanceSvc.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CalcLib.Maeda.Basis;
+using CalcLib.Maeda.Dispatcher;
+using CalcLib.Util;
+
+namespace CalcLib.Maeda.Finance
+{
+    /// <summary>
+    /// ファイナンスサービスのためのコンテキスト
+    /// </summary>
+    internal class FinanceContext : ICalcContext
+    {
+        /// <summary>
+        /// 証券コード4桁
+        /// </summary>
+        public string StockCode { get; set; }
+
+        /// <summary>
+        /// 通貨コード
+        /// </summary>
+        public string Currency { get; set; }
+
+        /// <summary>
+        /// 株価
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// いつ時点の株価か
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// エラーメッセージ
+        /// </summary>
+        public string ErrorMessage { get; set; }
+
+        public string DisplayText => ErrorMessage == null
+            ? $"[{StockCode}] {Price} {Currency}"  // TODO カンマ編集
+            : StockCode;
+
+        public string SubDisplayText => ErrorMessage ?? "";
+
+        public SvcState State { get; set; }
+    }
+
+    /// <summary>
+    /// ファイナンスサービス
+    /// </summary>
+    internal class FinanceSvc : SvcBase<FinanceContext>
+    {
+        public override string GetExtButtonText(int num) => num == 1 ? "株価" : null;
+
+        internal override FinanceContext _CreateContext() => new FinanceContext();
+
+        internal string CheckInput(string code)
+        {
+            var mr = Regex.Match(code, @"^\d,?\d\d\d$");
+            if (!mr.Success) throw new ApplicationException("INPUT ERROR");
+            return code.Replace(",", "");
+        }
+
+        public override void OnEnter(FinanceContext ctx, SvcSwichedEventArg arg)
+        {
+            ctx.StockCode = arg.PrevCtx.DisplayText;
+        }
+
+        public override bool TryButtonClick(FinanceContext ctx, CalcButton btn)
+        {
+            ctx.ErrorMessage = null;  // エラークリア
+
+            switch (btn)
+            {
+                case CalcButton.BtnExt1:
+                    if (ctx.State != SvcState.Unknown) return false;
+                    // 初回処理
+                    try
+                    {
+                        ctx.StockCode = CheckInput(ctx.StockCode);        // 入力チェック（エラー時は例外が出る）
+                        var sp = StockUtil.GetStockPrice(ctx.StockCode);  // 株価取得
+                        ctx.Price = sp.Price;  // 株価
+                        ctx.Currency = "JPY";  // 通貨は JPY 固定とする
+                        ctx.Date = sp.Date;    // 日時
+                    }
+                    catch (ApplicationException ex)
+                    {
+                        ctx.ErrorMessage = ex.Message;
+                    }
+                    catch (Exception ex)
+                    {
+                        ctx.ErrorMessage = "SCRAPING ERROR";
+                        Debugger.Break();
+                    }
+                    ctx.State = SvcState.Finished;
+                    break;
+                default:
+                    if (ctx.State == SvcState.Finished) return false;
+                    // 2回目以降
+                    // FIXME
+                    break;
+            }
+
+            return true;
+        }
+
+        protected override void OnExitSvc(FinanceContext ctx)
+        {
+            ctx.State = SvcState.Unknown;
+        }
+    }
+}
---
+++ CalcLibTest/Maeda/FinanceSvcTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcLib.Maeda;
+using CalcLib.Maeda.Finance;
+using System.Collections.Generic;
+using System.Linq;
+using CalcLib;
+
+namespace CalcLibTest.Maeda
+{
+    [TestClass]
+    public class FinanceSvcTest
+    {
+        class PrevContext : ICalcContext
+        {
+            public string DisplayText { get; set; }
+
+            public string SubDisplayText { get; set; }
+        }
+
+        FinanceContext ctx;
+        FinanceSvc svc;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            svc = new FinanceSvc();
+            ctx = svc._CreateContext();
+        }
+
+
+        [TestMethod]
+        public void TestFinanceMaeda_証券コード4桁のデータ引継ぎ()
+        {
+            svc.OnEnter(ctx, new CalcLib.Maeda.Dispatcher.SvcSwichedEventArg(new PrevContext
+            {
+                DisplayText = "1,301"
+            }));
+
+            // 株価ボタンを押す
+            var ret = svc.TryButtonClick(ctx, CalcLib.CalcButton.BtnExt1);
+            
+            Assert.IsTrue(ret);
+            Assert.AreEqual("", ctx.SubDisplayText);
+            Assert.AreEqual("[1301] 1000 JPY", ctx.DisplayText);
+
+            // 4 を押す
+            ret = svc.TryButtonClick(ctx, CalcLib.CalcButton.Btn4);
+
+            // false が返される
+            Assert.IsFalse(ret);
+            Assert.AreEqual("", ctx.SubDisplayText);
+        }
+    }
+}
