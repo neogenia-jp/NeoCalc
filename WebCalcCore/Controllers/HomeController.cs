b47863474e3fa845251990c5bc5675f7118d8c8c
haruyan-hopemucci <h.tomida@neogenia.co.jp>
2023-06-21 19:37:17 +0900

    feat: NeoCalcCore移植完了

---
+++ WebCalcCore/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using CalcLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NeoCalc.WebCalc.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace WebCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        const string _SESSION_KEY = "_session_key";
+        private IMemoryCache _memoryCache;
+
+        public HomeController(IMemoryCache memCache)
+        {
+            _memoryCache = memCache;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var svc = Factory.CreateService();
+            ViewBag.SvcClassName = svc.GetType().Name;
+            SetupExtButtons(svc as ICalcSvcEx);
+            // uuidを生成しセッションに保存する。このuuidはインメモリキャッシュのキーになる。
+            HttpContext.Session.SetString(_SESSION_KEY, Guid.NewGuid().ToString());
+            return View();
+        }
+
+        private void SetupExtButtons(ICalcSvcEx svc)
+        {
+            if (svc == null) return;
+            ViewBag.ExtButtonText = new[] { 0, 1, 2, 3 }.Select(i => svc.GetExtButtonText(i + 1)).ToArray();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string btn)
+        {
+            var sessionKey = HttpContext.Session.GetString(_SESSION_KEY) ?? string.Empty;
+            // sessionKeyが空の場合は最初のページを開いていないのでリダイレクトする
+            if (String.IsNullOrEmpty(sessionKey)){
+                Redirect("Index");
+            }
+            //var ctx = Session["Context"] as ICalcContext;
+            var ctx = GetObjectMemoryCache<ICalcContext>(sessionKey);
+            if (ctx == null)
+            {
+                ctx = Factory.CreateContext();
+                //Session["Context"] = ctx;
+                SetObjectMemoryCache<ICalcContext>(sessionKey, ctx);
+            }
+
+            var btnName = $"Btn{btn}";
+            CalcButton cb;
+            if (!Enum.TryParse(btnName, out cb))
+            {
+                throw new ApplicationException($"ボタン'{btnName}'が解釈できません。");
+            }
+
+            var svc = Factory.CreateService();
+            svc.OnButtonClick(ctx, cb);
+
+            return new JsonResult( ctx );
+        }
+
+        private T? GetObjectMemoryCache<T>(string key)
+        {
+            _memoryCache.TryGetValue<T>(key, out var value);
+            return value;
+        }
+        private void SetObjectMemoryCache<T>(string key, T value)
+        {
+            _memoryCache.Set<T>(key, value);
+        }
+    }
+}
---
+++ WebCalcCore/Program.cs
@@ -0,0 +1,39 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// ASP.NET Core で Sessionを有効にするための設定
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(2);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// インメモリキャッシュ有効設定
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// ASP.NET Core で Sessionを有効にするための設定
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
