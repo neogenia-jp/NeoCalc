3f88a2b89aa0018862b1790d9efdf4c373316d5c
KeitaTakao <k.takao@neogenia.co.jp>
2023-07-20 09:38:14 +0900

    feat: ディスプレイの管理を行なうメソッド追加

--- CalcLibCore/Takao/CalcContext.cs
+++ CalcLibCore/Takao/CalcContext.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -8,8 +7,76 @@ using System.Threading.Tasks;
 
 namespace CalcLib.Takao
 {
-    internal class CalcContextTakao : CalcContext
+    internal class CalcContext : ICalcContext
     {
+        public string DisplayText { get; set; } = "0";
+        public string SubDisplayText { get; set; } = "0";
+        public string left = "0";
+        public string right = "0";
+        public string right_memo = "0";
+
+        public ICalcStrategy? operatorMode;
+
+        public override string ToString() => $"display: {DisplayText} subdispplay: {SubDisplayText}";
+
+        // mainの操作
+        public void ApplyDisplayText()
+        {
+            FormatOperand();
+            DisplayText = (right == "0") ? left : right;
+            debugContext();
+        }
+
+        public void FormatOperand()
+        {
+            right = Decimal.Parse(right).ToString();
+            left = Decimal.Parse(left).ToString();
+        }
+
+        public (decimal, decimal) ParseOperand()
+        {
+            return (Decimal.Parse(left), Decimal.Parse(right));
+        }
+
+        public void SetOperator(ICalcStrategy mode)
+        {
+            operatorMode = mode;
+
+            // 1+1+1+=3のために実行する。
+            if (operatorMode != null)
+            {
+                right_memo = right;
+                operatorMode.Execute(this);
+            }
+            else
+            {
+                left = right;
+                right = "0";
+            }
+        }
+
+        public void debugContext()
+        {
+            Console.WriteLine(ToString());
+            Console.WriteLine($"left: {left}, right: {right}, right_memo: {right_memo}, operator: {operatorMode?.ToString()}");
+        }
+
+        // subの操作
+        // public void ApplySubDisplayText(CalclatorSvc svc)
+        // {
+        //     SubDisplayText = "";
+        //     // ButtonHistory.ForEach(btn => svc.ExecuteSub(btn));
+        // }
+
+        // display 初期化
+        public void Clear()
+        {
+            left = "0";
+            right = "0";
+            DisplayText = "0";
+            SubDisplayText = "0";
+            operatorMode = null;
+        }
     }
 }
 
