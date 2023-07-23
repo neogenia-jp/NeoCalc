212fe1af495906f452762a87a3d8f969ff05c388
Haruaki Tomida <h.tomida@neogenia.co.jp>
2023-07-23 16:49:16 +0900

    wip: おみくじコンテキストのベースになるコード

---
+++ CalcLibCore/Tomida/OmikujiContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CalcLib;
+using CalcLibCore.Tomida.Extensions;
+
+namespace CalcLibCore.Tomida
+{
+	public class OmikujiContext: ICalcContext
+	{
+        private static string[] _SUITS_DEFAULT = new string[] { "大吉", "中吉", "小吉", "凶" };
+
+        private string[] _suits;
+
+		public OmikujiContext()
+		{
+            _suits = _SUITS_DEFAULT.Shuffle();
+		}
+
+        public string DisplayText => DisplayTextImpl();
+
+        private string DisplayTextImpl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SubDisplayText => SubDisplayTextImpl();
+
+        private string SubDisplayTextImpl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CalcConstants.State State => GetState();
+
+        private CalcConstants.State GetState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
