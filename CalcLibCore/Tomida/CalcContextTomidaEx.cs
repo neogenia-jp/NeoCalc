021d56f1e8f50254095174eaeaa829eba32cc76f
Haruaki Tomida <h.tomida@neogenia.co.jp>
2023-07-23 17:04:09 +0900

    feat: コンテキストの挿げ替えが可能な抽象コンテキストを実装

---
+++ CalcLibCore/Tomida/CalcContextTomidaEx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CalcLib;
+
+namespace CalcLibCore.Tomida
+{
+	/// <summary>
+	/// コンテキストをスタックできるメディエーターコンテキスト
+	/// 現在のコンテキストは Current プロパティで呼び出す。
+	/// </summary>
+	public class CalcContextTomidaEx: ICalcContext
+	{
+		private List<ICalcContext> _contextStack = new();
+
+		public CalcContextTomidaEx()
+		{
+			_contextStack.Add(new CalcContextTomida());
+		}
+
+		/// <summary>
+		/// 現在スタック上で一番上に存在するコンテキストを取得する
+		/// </summary>
+		public ICalcContext Current => _contextStack.Last();
+
+		/// <summary>
+		/// 新しいコンテキストスタックを積む
+		/// </summary>
+		/// <param name="ctx"></param>
+		public void StackContext(ICalcContext ctx) => _contextStack.Add(ctx);
+
+		/// <summary>
+		/// Currentコンテキストを除去する
+		/// </summary>
+		public void UnstackContext() => _contextStack.Remove(Current);
+
+		/// <summary>
+		/// DisplayTextはCurrentコンテキストに委譲する
+		/// </summary>
+		public string DisplayText => Current.DisplayText;
+
+		/// <summary>
+		/// SubDisplayTextはCurrentコンテキストに委譲する
+		/// </summary>
+		public string SubDisplayText => Current.SubDisplayText;
+    }
+}
+
--- CalcLibCore/Tomida/CalcSvcTomida.cs
+++ CalcLibCore/Tomida/CalcSvcTomida.cs
@@ -12,19 +12,24 @@ namespace CalcLib.Tomida
 {
     internal class CalcSvcTomida : ICalcSvc
     {
-        public virtual ICalcContext CreateContext() => new CalcContextTomida();
+        public virtual ICalcContext CreateContext() => new CalcContextTomidaEx();
 
         public virtual void OnButtonClick(ICalcContext ctx0, CalcButton btn)
         {
-            var ctx = ctx0 as CalcContextTomida;
+            var ctxEx = ctx0 as CalcContextTomidaEx;
+            if (ctxEx is null) return;  // コンテキストがnullだったら何もしない
+            var ctx = ctxEx.Current;
             if (ctx is null) return;    // コンテキストがnullだったら何もしない
             Debug.WriteLine($"Button Clicked {btn}, context={ctx}");
+            if(ctx.GetType() == typeof(CalcContextTomida))
+            {
+                // 押されたボタンに対応するコマンドオブジェクトをファクトリーで生成
+                // Executeする
+                var command = ButtonCommandFactory.Create(btn);
+                if (command is null) return;    // ファクトリのcommand生成に失敗したら何もしない
+                command.Execute((CalcContextTomida)ctx);
+            }
 
-            // 押されたボタンに対応するコマンドオブジェクトをファクトリーで生成
-            // Executeする
-            var command = ButtonCommandFactory.Create(btn);
-            if (command is null) return;    // ファクトリのcommand生成に失敗したら何もしない
-            command.Execute(ctx);
         }
     }
 }
