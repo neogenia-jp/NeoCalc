69ce79e6ee12de2ca41ab6ce23b6806ef9a297f1
TomoProg <t.yamamoto@neogenia.co.jp>
2025-07-10 13:47:28 +0900

    feat: Stateパターンで挑戦
    TestPlusの１個目のassertをクリア

--- CalcLibCore/Yamamoto3/CalcSvcYamamoto3.cs
+++ CalcLibCore/Yamamoto3/CalcSvcYamamoto3.cs
@@ -14,6 +14,26 @@ namespace CalcLib.Yamamoto3
         public virtual void OnButtonClick(ICalcContext ctx0, CalcButton btn)
         {
             var ctx = ctx0 as CalcContextYamamoto3;
+            if(btn == CalcButton.Btn0 || btn == CalcButton.Btn1 || btn == CalcButton.Btn2 ||
+               btn == CalcButton.Btn3 || btn == CalcButton.Btn4 || btn == CalcButton.Btn5 ||
+               btn == CalcButton.Btn6 || btn == CalcButton.Btn7 || btn == CalcButton.Btn8 ||
+               btn == CalcButton.Btn9)
+            {
+                ctx.State.InputNumber(ctx, btn);
+            }
+            else if(btn == CalcButton.BtnPlus || btn == CalcButton.BtnMinus ||
+                    btn == CalcButton.BtnDivide || btn == CalcButton.BtnMultiple)
+            {
+                ctx.State.InputOperator(ctx, btn);
+            }
+            else if(btn == CalcButton.BtnEqual)
+            {
+                ctx.State.InputEqual(ctx, btn);
+            }
+            else
+            {
+                // その他のボタンは何もしない
+            }
             Debug.WriteLine($"Button Clicked {btn}, context={ctx}");
         }
     }
