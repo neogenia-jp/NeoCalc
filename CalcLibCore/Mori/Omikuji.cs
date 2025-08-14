6cdd43d8f64adadc8d25fe3d8393fad0c0fbe643
mori yuji <y.mori@neogenia.co.jp>
2025-08-14 11:58:23 +0900

    OmikujiクラスをDRYに整理

--- CalcLibCore/Mori/Omikuji.cs
+++ CalcLibCore/Mori/Omikuji.cs
@@ -3,75 +3,67 @@ namespace CalcLib.Mori
     // おみくじのクラス
     internal class Omikuji
     {
+        private static readonly string DefaultMainText = "[1 ] [2 ] [3 ] [4 ]";
+        private static readonly string DefaultSubText  = "おみくじを選択して下さい";
         private static readonly string[] _kuji = { "大吉", "中吉", "小吉", "凶　" };
         private Dictionary<int, string> _map = new();
 
         internal int? _selected;
         // おみくじが選択済みかどうか
         internal bool HasSelected => _selected.HasValue;
-        internal string _mainDisplay = "[1 ] [2 ] [3 ] [4 ]";
-        internal string _subDisplay = "おみくじを選択して下さい";
 
-        public Omikuji()
-        {
-            // 初期化とシャッフル
-            Init();
-        }
-
-        // trueでおみくじ終了
+        /// <summary>
+        /// 入力ボタンを処理する。おみくじモードの継続/終了を判定する。
+        /// </summary>
+        /// <param name="context">電卓コンテキスト</param>
+        /// <param name="btn">ボタン</param>
+        /// <returns>モードを終了時 true、継続する場合は false</returns>
         public bool Accept(CalcContextExtend context, CalcButton btn)
         {
-            if (btn.IsClear() || btn.IsCE() || btn.IsOmikuji())
-            {
-                Init();
-                return true;
-            } 
-            else if (HasSelected)
-            {
-                Init();
-                return true;
-            }
-            else if (btn.IsOmikujiSelect())
+            if (btn.IsClear() || btn.IsCE() || btn.IsOmikuji()) return Exit();
+            if (HasSelected) return Exit();
+            if (btn.IsOmikujiSelect())
             {
                 _selected = (int)btn;
                 return false;
-            }  
+            }
             
             // その他のキーは無効
             return false;
         }
 
-        internal void Shuffle()
+        private void Shuffle()
         {
             _map = _kuji
                 .OrderBy(_ => Random.Shared.Next())
                 .Select((name, idx) => (idx + 1, name))
                 .ToDictionary(t => t.Item1, t => t.name);
         }
+
+        /// <summary>
+        /// おみくじの初期化
+        /// </summary>
         internal void Init()
         {
-            _mainDisplay = "[1 ] [2 ] [3 ] [4 ]";
-            _subDisplay = "おみくじを選択して下さい";
             _selected = null;
             Shuffle();
         }
-        
+
+        // おみくじモードの終了条件で呼ばれる共通メソッド
+        private bool Exit()
+        {
+            Init();
+            return true;
+        }
+
         // 表示用のテキストとモードを返す
         internal DisplaySource RowDisplay()
         {
-            UIMode mode = UIMode.Omikuji;
             if (_selected.HasValue)
             {
-                _mainDisplay = string.Join(" ", _map.Values);
-                _subDisplay = $"本日の運勢は「{_map[_selected.Value]}」です";
-                mode = UIMode.OmikujiEnd;
+                return new DisplaySource(string.Join(" ", _map.Values), $"本日の運勢は「{_map[_selected.Value]}」です", UIMode.OmikujiEnd);
             }
-            else
-            {
-                _mainDisplay = "[1 ] [2 ] [3 ] [4 ]";
-                _subDisplay = "おみくじを選択して下さい";
-            }
-            return new DisplaySource(_mainDisplay, _subDisplay, mode);
+            return new DisplaySource(DefaultMainText, DefaultSubText, UIMode.Omikuji);
         }
     }
 }
